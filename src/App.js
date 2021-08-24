7488777ffb222deef887dcea235bfb0dbb501f1d
LottoRed <lotto@lotto-red.com>
2021-08-23 17:31:14 -0700

    broken

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-     <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Pornography
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Pornography
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
